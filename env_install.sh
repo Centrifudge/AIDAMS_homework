aab73955291f2ca9f13091140b29f4c62df3f63f
Melchior de Polignac <melchiordepolignac@gmail.com>
2026-09-16 23:28:36 +0200

    add os detection using the OSTYPE variable

--- env_install.sh
+++ env_install.sh
@@ -7,7 +7,18 @@ pretty_print() {
     printf "\n"
 }
 
+# figure out which os we are on so we can pick the right package manager
+detect_os() {
+    case "$OSTYPE" in
+        darwin*)    OS="macos";  PM="brew" ;;
+        linux)      OS="linux";  PM="apt" ;;
+        *)          OS="unknown"; PM="unknown" ;;
+    esac
+}
+
 main() {
+    detect_os
+    pretty_print "detected OS: $OS, package manager: $PM"
     pretty_print "dev environment setup starting"
 }
 
