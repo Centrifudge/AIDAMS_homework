ad5468aa7846829ab66b373f31ef8c8940101ba7
Melchior de Polignac <melchiordepolignac@gmail.com>
2026-09-16 23:28:45 +0200

    add python version check

--- env_install.sh
+++ env_install.sh
@@ -16,10 +16,20 @@ detect_os() {
     esac
 }
 
+# check that python3 is around, install it if it is missing
+check_python() {
+    if command -v python3 >/dev/null 2>&1; then
+        pretty_print "python3 is installed: $(python --version)"
+    else
+        pretty_print "python3 not found, will install it"
+    fi
+}
+
 main() {
     detect_os
     pretty_print "detected OS: $OS, package manager: $PM"
     pretty_print "dev environment setup starting"
+    check_python
 }
 
 main
